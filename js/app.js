b53cfc28c75c12a6f76141b44798407b1df360e2
toran billups <toranb@gmail.com>
2013-07-10 18:29:36 -0500

    init commit

---
+++ js/app.js
@@ -0,0 +1,52 @@
+App = Ember.Application.create();
+
+App.Router.map(function() {
+    this.resource("people", { path: "/" });
+});
+
+App.PeopleRoute = Ember.Route.extend({
+    model: function() {
+        return App.Person.find();
+    }
+});
+
+App.PeopleController = Ember.ArrayController.extend({
+    addPerson: function() {
+        var person = {
+            firstName: this.get('firstName'),
+            lastName: this.get('lastName')
+        };
+        App.Person.add(person);
+    },
+    deletePerson: function(person) {
+        App.Person.remove(person);
+    }
+});
+
+App.Person = Ember.Object.extend({
+    firstName: '',
+    lastName: '',
+    fullName: function() {
+        var firstName = this.get('firstName');
+        var lastName = this.get('lastName');
+        return firstName + ' ' + lastName;
+    }.property('firstName', 'lastName')
+});
+
+App.Person.reopenClass({
+    people: [],
+    add: function(hash) {
+        var person = App.Person.create(hash);
+        this.people.pushObject(person);
+    },
+    remove: function(person) {
+        this.people.removeObject(person);
+    },
+    find: function() {
+        this.people = [
+            App.Person.create({firstName: 'matt', lastName: 'morrison'}),
+            App.Person.create({firstName: 'toran', lastName: 'billups'})
+        ];
+        return this.people;
+    }
+});
